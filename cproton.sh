6556ba90c8382061eb7477fe53099cf1ee437b48
flubberding <58474155+flubberding@users.noreply.github.com>
2019-12-08 18:16:45 +0000

    CProton 0.2
    > Code has been restructured and redone for a big part.
    > Added option to restart Steam either automatically or by prompt
    > Added option to install Proton builds without confirmation-prompt. Running the script without parameters will only install when the latest version is not yet installed.
    > Added instructions to the releases list function (./cproton.sh -l)
    > Added instructions for options and changed filename to cproton.sh

---
+++ cproton.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+baseuri="https://github.com/GloriousEggroll/proton-ge-custom/releases/download"
+latesturi="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
+parameter="${1}"
+dstpath="$HOME/.steam/root/compatibilitytools.d" #### Destinationforlder of the Proton installations
+restartSteam=2
+autoInstall=false
+#### Set restartSteam=0 to not restart steam after installing Proton (Keep process untouched)
+#### Set restartSteam=1 to autorestart steam after installing Proton
+#### Set restartSteam=2 to to get a y/n prompt asking if you want to restart Steam after each installation.
+
+#### Set autoInstall=true to skip the installation prompt and install the latest not-installed, or any forced Proton GE builds
+#### Set autoInstall=false to display a installation-confirmation prompt when installing a Proton GE build
+
+# ########################################## Custom Proton GloriousEggroll Installscript 0.2 ##########################################
+# Disclaimer: Subversions like the MCC versions of Proton 4.21-GE-1, will install as it's main version and not install separately.
+# For now, this may result in false "not installed"-detections or errors while force installing a specific subversion.
+
+PrintReleases() {
+  echo "----------Description----------"
+  echo ""
+  echo "Run './getProtonGE.sh [VersionName]'"
+  echo "to download specific versions."
+  echo ""
+  echo "------------Releases------------"
+  curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases | grep -H "tag_name" | cut -d \" -f4
+  echo "--------------------------------"
+}
+
+InstallProtonGE() {
+  rsp="$(curl -sI "$url" | head -1)"
+  echo "$rsp" | grep -q 302 || {
+    echo "$rsp"
+    exit 1
+  }
+
+  [ -d "$dstpath" ] || {
+    mkdir "$dstpath"
+    echo [Info] Created "$dstpath"
+  }
+  curl -sL "$url" | tar xfzv - -C "$dstpath"
+}
+
+RestartSteam() {
+  if [ "$(ps -A | grep steam)" != "" ]; then
+    echo "Restarting Steam"
+    pkill -TERM steam #restarting Steam
+    sleep 5s
+    nohup steam </dev/null &>/dev/null &
+  fi
+}
+
+RestartSteamCheck() {
+  if [ "$(ps -A | grep steam)" != "" ]; then
+    if [ $restartSteam == 2 ]; then
+      read -p "Do you want to restart Steam? <y/N> " prompt
+      if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+        RestartSteam
+      else
+        exit 2
+      fi
+    elif [ $restartSteam == 0 ]; then
+      exit 0
+    fi
+    RestartSteam
+  fi
+}
+
+InstallationPrompt() {
+  if [ "$autoInstall" = true ]; then
+    if [ ! -d $dstpath/Proton-$version ]; then
+      InstallProtonGE
+    fi
+  else
+    read -p "Do you want to try to download and (re)install this release? <y/N> " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+      InstallProtonGE
+    else
+      echo "Operation canceled"
+      exit 0
+    fi
+  fi
+}
+
+if [ -z "$parameter" ]; then
+  version="$(curl -s $latesturi | egrep -m1 "tag_name" | cut -d \" -f4)"
+  url=$(curl -s $latesturi | egrep -m1 "browser_download_url.*Proton" | cut -d \" -f4)
+  if [ -d $dstpath/Proton-$version ]; then
+    echo "Proton $version is the latest version and is already installed."
+  else
+    echo "Proton $version is the latest version and is not installed yet."
+  fi
+elif [ $parameter == "-l" ]; then
+  PrintReleases
+else
+  url=$baseuri/"$parameter"/Proton-"$parameter".tar.gz
+  if [ -d $dstpath/Proton-$parameter ]; then
+    echo "Proton $parameter is already installed."
+  else
+    echo "Proton $parameter is not installed yet."
+  fi
+fi
+
+if [ ! "$parameter" == "-l" ]; then
+  InstallationPrompt
+  RestartSteamCheck
+fi
