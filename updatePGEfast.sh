cc2220ceb02084ecca438bf813961176e032eff7
ArtyIF <artyomisflash@mail.ru>
2023-01-01 12:31:35 +0300

    Remove unused variable from non-interactive script

--- updatePGEfast.sh
+++ updatePGEfast.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-baseuri="https://github.com/GloriousEggroll/proton-ge-custom/releases/download"
 latesturi="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
 dstpath="$HOME/.steam/root/compatibilitytools.d"
 
